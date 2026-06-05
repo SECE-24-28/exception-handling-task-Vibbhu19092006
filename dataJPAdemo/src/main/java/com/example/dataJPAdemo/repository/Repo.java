10b10caff5502b1cbfac528013a09f1355e4e2c0
Vibbhu19092006 <vibbhuashokkumar@gmail.com>
2026-06-05 09:30:27 +0530

    Delete dataJPAdemo directory

--- dataJPAdemo/src/main/java/com/example/dataJPAdemo/repository/Repo.java
+++
@@ -1,7 +0,0 @@
-package com.example.dataJPAdemo.repository;
-
-import com.example.dataJPAdemo.model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Repo extends JpaRepository<Student,Integer> {
-}
